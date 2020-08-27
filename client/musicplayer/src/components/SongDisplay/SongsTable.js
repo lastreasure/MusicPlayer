f73ec51552ec27ebfd49f409e0c746ac7f646b2d
lastreasure <letitia.treasure@vodafone.com>
2020-08-27 22:20:53 +0100

    comments

--- client/musicplayer/src/components/SongDisplay/SongsTable.js
+++ client/musicplayer/src/components/SongDisplay/SongsTable.js
@@ -189,8 +189,9 @@ visuallyHidden: {
 }));
 
 const SongsTable = (allSongs = []) => {
-        
+    
     const classes = useStyles();
+    // Setting react hook states
     const [order, setOrder] = React.useState('asc');
     const [orderBy, setOrderBy] = React.useState('title');
     const [selected, setSelected] = React.useState([]);
